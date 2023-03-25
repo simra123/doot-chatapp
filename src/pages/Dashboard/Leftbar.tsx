cd4e3d64b9cff68ad255ccc6ede008317367ba07
simra123 <simrafcb@gmail.com>
2023-03-25 20:40:11 +0500

    first commit

---
+++ src/pages/Dashboard/Leftbar.tsx
@@ -0,0 +1,89 @@
+import React, {useEffect } from "react";
+import { TabContent, TabPane } from "reactstrap";
+
+// hooks
+import { useRedux } from "../../hooks/index";
+import { useNavigate } from "react-router-dom";
+// constants
+import { TABS } from "../../constants/index";
+
+// component
+import Profile from "./Profile/index";
+import Chats from "./Chats/index";
+import Contacts from "./Contacts/index";
+import Calls from "./Calls/index";
+import Bookmark from "./Bookmark/index";
+import Settings from "./Settings/index";
+
+interface LeftbarProps {}
+const Leftbar = (props: LeftbarProps) => {
+  // global store
+  const { useAppSelector } = useRedux();
+
+  const { activeTab } = useAppSelector(state => ({
+    activeTab: state.Layout.activeTab,
+  }));
+
+const  activePage : string =  activeTab?.split("-")[1]
+const navigate = useNavigate()
+
+useEffect(()=> {
+if(activePage){
+  navigate(`/${activePage}`)
+}
+}, [activePage])
+
+  return (
+    <>
+      {/* start chat-leftsidebar */}
+      <div className="chat-leftsidebar">
+        <TabContent activeTab={activeTab}>
+          {/* Start Profile tab-pane */}
+          <TabPane
+            tabId={TABS.USERS}
+            role="tabpanel"
+          >
+            <Profile />
+          </TabPane>
+
+          <TabPane
+            tabId={TABS.CHAT}
+            role="tabpanel"
+          >
+            <Chats />
+          </TabPane>
+
+          <TabPane
+            tabId={TABS.CONTACTS}
+            role="tabpanel"
+          >
+            <Contacts />
+          </TabPane>
+
+          <TabPane
+            tabId={TABS.CALLS}
+            role="tabpanel"
+          >
+            <Calls />
+          </TabPane>
+
+          <TabPane
+            tabId={TABS.BOOKMARK}
+            role="tabpanel"
+          >
+            <Bookmark />
+          </TabPane>
+
+          <TabPane
+            tabId={TABS.SETTINGS}
+            role="tabpanel"
+          >
+            <Settings />
+          </TabPane>
+        </TabContent>
+      </div>
+    </>
+  );
+};
+
+export default Leftbar;
